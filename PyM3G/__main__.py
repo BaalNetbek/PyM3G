51d6108ab4f23281b4959b78fcd8c55201dd1796
Baal <zzztomek@o2.pl>
2025-05-13 00:50:37 +0200

    unfix reader argument

--- PyM3G/__main__.py
+++ PyM3G/__main__.py
@@ -8,7 +8,7 @@ from PyM3G.reader import M3GReader
 
 c = console.Console()
 
-m3g = M3GReader(r"C:\Users\Ismail\Desktop\GALAXY_ON_FIRE\GOF2 JAVA\gof2m3g\3d_planet.m3g", "WARNING")
+m3g = M3GReader(argv[1], "WARNING")
 idx = 0
 for obj in m3g.objects:
     c.print(f"({idx}) {obj}")
