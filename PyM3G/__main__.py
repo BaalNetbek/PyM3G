c0c1c728e3d0dfac734947d1bb293dc27f98df1f
BaalNetbek <157546829+BaalNetbek@users.noreply.github.com>
2026-07-08 02:18:32 +0200

    make rich optional

--- PyM3G/__main__.py
+++ PyM3G/__main__.py
@@ -4,19 +4,31 @@ Dumps all data from the specified m3g file when the module is called directly
 
 from sys import argv
 import logging
-from rich import console
-from rich.logging import RichHandler
 from pym3g.file import M3GFile
 
-logging.basicConfig(
-    level=logging.NOTSET,
-    format="%(message)s",
-    datefmt="[%X]",
-    handlers=[RichHandler()],
-)
-m3g = M3GFile(argv[1], log_level=logging.DEBUG)
-c = console.Console()
 
+try:
+    from rich import console
+    from rich.logging import RichHandler
+    logging.basicConfig(
+        level=logging.NOTSET,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler()],
+    )
+    c = console.Console()
+except ImportError:
+    logging.basicConfig(
+        level=logging.NOTSET,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%H:%M:%S",
+        handlers=[logging.StreamHandler()],
+    )
+        
+m3g = M3GFile(argv[1], log_level=logging.DEBUG)
 for i, obj in enumerate(m3g.objects, 1):
-    c.print("({0}) {1}".format(i, obj))
+    if 'c' in locals():
+        c.print("({0}) {1}".format(i, obj))
+    print("({0}) {1}".format(i, obj))
+
 
